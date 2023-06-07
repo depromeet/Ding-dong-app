8175937691ce7e0a81f82a708db2722bf103c103
문혜현 <55529617+hyehyeonmoon@users.noreply.github.com>
2023-06-08 00:47:08 +0900

    Dep 43 Add lint rule, husky and folder structure (#3)
    * Feat: Add node version and ruby version
    
    * Feat: eslint rule
    
    * Feat: prettier rule
    
    * Feat: husky pre-commit rule
    
    * Feat: typescript config
    
    * Feat: formatting existed files by prettier
    
    * Feat: fold structure
    
    * Feat: Add CODEOWNER file
    
    ---------
    
    Co-authored-by: hyehyeon-moon <hyehyeon.moon@linecorp.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
   singleQuote: true,
-  trailingComma: 'all',
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "lf",
 };
