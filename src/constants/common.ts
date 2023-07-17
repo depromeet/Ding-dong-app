5dae2e998bb4f0d5ce80a8f249303d138c8015ce
darae lee <61297852+darae07@users.noreply.github.com>
2023-07-18 01:06:25 +0900

    feat: url 변경 (#6)

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,3 @@
 // http의 경우 android 9 이상에서는 https로만 통신이 가능하여 웹뷰 동작하지 않음
 // 로컬에서는 localhost로 바꾸고 ios 환경에서 테스트 해보세요
-export const BASE_URI = 'https://naver.com/';
+export const BASE_URI = 'https://ding-dong-planet.com/';
